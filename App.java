4a700ed0c03b2a1d9e4d07f0dd5b304640267f57
mia71719 <mia71719@gmail.com>
2025-12-23 15:09:30 +0800

    Update App.java

--- App.java
+++ App.java
@@ -5,9 +5,9 @@ public static void main(String[] args) throws Exception
     byte[] array = inputString.getBytes(StandardCharsets.UTF_8);
     
     for (int i = array.length - 1; i >= 0; i--) 
-        {     newStr.append((char) (array[i] - 1));
-    System.out.println((char) (array[i] - 1)));
-        }String result = newStr.toString();
+         newStr.append((char) (array[i] - 1));
+   
+    String result = newStr.toString();
     System.out.println("Transformed String: " + result);
     boolean matches = Pattern.matches("[A-Z]{3}[0-9]{3}", inputString);
     System.out.println("Matches pattern [A-Z]{3}[0-9]{3}: " + matches);
