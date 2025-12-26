14edc88c37c65731027d214d097d82457565ee90
mia71719 <mia71719@gmail.com>
2025-12-26 16:19:24 +0800

    sender java code
    Refactored the readFile method and added a new Sender class for pinging servers.

--- err.java
+++ err.java
@@ -1,63 +1,28 @@
-private void readFile()
-	{
-		try 
-		{     DbEnv dbEnv = new DbEnv();
-		      SQLExecuter sqlExce = dbEnv.getTestTpasExecuter("DW");
-		      String customer_id = "570";
-		      StringBuffer sb = new StringBuffer();
-		      StringBuffer sb_data = new StringBuffer();
-			  String path = "C:\\MH_USE\\my_request\\insert\\";
-			  File myObj = new File(path+customer_id+"_insert.txt");
-			
-		      Scanner myReader = new Scanner(myObj);
-		     
-		      while (myReader.hasNextLine()) 
-		      {
-		        String data = myReader.nextLine();
-		        if (data.startsWith("add prod_info:"))
-		        	prodInfoList.add(data);
-		      }
-		      myReader.close();
-		      
-		     
-		      for (String prodInfo : prodInfoList)
-		      {
-		    	  String[] s =(prodInfo.substring(prodInfo.indexOf(":")+1)).split(",");
-		    	  String prdgrp_id = s[0];
-		    	  String ec_type = s[1];
-		    	  String ec_no = s[2];
-		    	  String op_name = s[3];
-		    	  sb_data.append(prdgrp_id+","+ec_type+","+ec_no+","+op_name+"\n");
-		    	  String sql = "select * from prod_info where prdgrp_id='"+prdgrp_id+"' and ec_type='"+ec_type+"'"+ " and ec_no='"+ec_no+"' and op_name='"+op_name+"' and waf_sampling_rule='' ";
-		    	 // System.out.println(sql);
-		    	  RowList rl = sqlExce.doQuery(sql);
-		    	  if (rl != null && rl.getCount() == 1)
-		    	  { 
-		    		  String logSql = "update prod_info set waf_sampling_rule='1:NA(Full wafer list to test)' where prdgrp_id='"+prdgrp_id+"' and ec_type='"+ec_type+"'"+ " and ec_no='"+ec_no+"' and op_name='"+op_name+"' and waf_sampling_rule=''  ";
-		    		  sb.append(logSql + "\n");
-		    		  String update = "update prod_info set waf_sampling_rule='1:NA(Full wafer list to test)' where prdgrp_id=? and ec_type=? and ec_no=? and op_name=? and waf_sampling_rule=''  ";
-		    		  objList.add(new Object[]{prdgrp_id, ec_type, ec_no, op_name});
-		    	  }
-		    	  
-		    	  
-		      }
-		      System.out.println(sb.toString());
-		      System.out.println(objList.size() +"==> prodInfoList =" + prodInfoList.size());
-		      
-		      
-		      String bakPath = "C:\\MH_USE\\my_request\\insert\\prod_info\\";
-			  File prod = new File(bakPath+customer_id+".txt");
-		      if (prod.createNewFile()) 
-		        System.out.println("File created: " + prod.getName());
-		      else 
-		        System.out.println("File already exists.");
-		      
-		      FileWriter myWriter = new FileWriter(bakPath+customer_id+".txt");
-		      myWriter.write(sb_data.toString());
-		      myWriter.close();
-		}
-		catch (Exception e)
-		{
-			
-		}
-	}
+mport java.io.*;
+import java.util.Scanner;
+
+public class Sender {
+    public static void main(String[] args) 
+    {
+             Scanner scanner = new Scanner(System.in);
+             System.out.print("請輸入要 Ping 的伺服器位址: ");
+             String ip = scanner.nextLine();
+
+        // 漏洞點：直接將使用者輸入拼接到系統指令中
+        // 如果使用者輸入 "8.8.8.8 && dir" (Windows) 或 "8.8.8.8 ; ls" (Linux)
+        // 系統會先執行 ping，接著執行後面的惡意指令
+        String command = "ping -n 1 " + ip; 
+
+        try {
+            Process process = Runtime.getRuntime().exec(command);
+            BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
+
+            String line;
+            while ((line = reader.readLine()) != null) {
+                System.out.println(line);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
